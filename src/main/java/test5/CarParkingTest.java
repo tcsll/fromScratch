2fde4b8cdfa40b6e6de833532d1fec66bfbe8ea4
shill <443973502@qq.com>
2020-04-07 14:55:16 +0800

    '智能停车场(未实现)'

---
+++ src/main/java/test5/CarParkingTest.java
@@ -0,0 +1,13 @@
+package test5;
+
+/**
+ * @author ：shill
+ * @date ：Created in 2020/4/7 14:52
+ * @description :
+ * 实现智能停车场
+ * 1) 停车场中有三个停车位。
+ * 2) 车辆可以实现停车(假定停车时间为10秒)和离开停车位的动作。
+ * 3) 使用多线程模拟实现多辆车停车。停车成功显示:xxx号车成功停止在xxx号停车位；如果该停车位已满则停车失败。
+ */
+public class CarParkingTest {
+}
